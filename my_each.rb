bbc95eed71032fb636dc561becc9d4b85fde37b5
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-08 05:29:49 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,9 @@
 def my_each(collection)# put argument(s) here
   # code here
   i=0
-  while i < collection.size 
+  while i < collection.size
     yield collection[i]
     i+=1
   end
   collection
 end
-
